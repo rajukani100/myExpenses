c05212ff83a43aad2a3451eee087026842096774
Raj Ukani <rajukani100@gmail.com>
2024-09-13 17:08:33 +0530

    added noteScreen file

---
+++ lib/screens/noteScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class noteScreen extends StatelessWidget {
+  const noteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
