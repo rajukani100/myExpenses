5ac5fac288a5fb8346dc76bb6a22885084625427
Raj Ukani <rajukani100@gmail.com>
2024-09-14 09:51:30 +0530

    designed noteScreen

--- lib/screens/noteScreen.dart
+++ lib/screens/noteScreen.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:my_expense/main.dart';
+import 'package:my_expense/services/cloud_data.dart';
+import 'package:my_expense/utils/color.dart';
+import 'package:my_expense/utils/responsive.dart';
 
 class noteScreen extends StatelessWidget {
-  const noteScreen({super.key});
+  noteScreen({super.key}) {}
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        SizedBox(
+          height: height15 * .75,
+        ),
+        Text(
+          "ExpenseNote",
+          style: TextStyle(
+              fontFamily: 'Gotham',
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.w600,
+              fontSize: font15 * 1.5,
+              color: mainColor),
+        ),
+        SizedBox(
+          height: height15 * .75,
+        ),
+        Expanded(
+          child: Obx(() => TextFormField(
+                controller: helperGetx.noteController.value,
+                onChanged: (value) async {
+                  await cloudData
+                      .writeNotes(helperGetx.noteController.value.text);
+                },
+                style: TextStyle(fontSize: 18),
+                maxLines: null,
+                expands: true,
+                textAlignVertical: TextAlignVertical.top,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15)),
+                      borderSide: BorderSide(width: 1, color: mainGrey)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15)),
+                      borderSide: BorderSide(width: 1, color: mainGrey)),
+                  hintText: 'type your expense note here....  ',
+                ),
+              )),
+        )
+      ],
+    );
   }
 }
