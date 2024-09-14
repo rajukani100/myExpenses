b0389eab27f9c1818e89e9123f9e774354b1c868
Raj Ukani <rajukani100@gmail.com>
2024-09-14 10:40:58 +0530

    bug fixes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -144,17 +144,14 @@ class myLogin extends StatelessWidget {
                         // Trigger the authentication flow
                         final GoogleSignInAccount? googleUser =
                             await GoogleSignIn().signIn();
-
                         // Obtain the auth details from the request
                         final GoogleSignInAuthentication? googleAuth =
                             await googleUser?.authentication;
-
                         // Create a new credential
                         final credential = GoogleAuthProvider.credential(
                           accessToken: googleAuth?.accessToken,
                           idToken: googleAuth?.idToken,
                         );
-
                         // Once signed in, return the UserCredential
                         return await FirebaseAuth.instance
                             .signInWithCredential(credential);
